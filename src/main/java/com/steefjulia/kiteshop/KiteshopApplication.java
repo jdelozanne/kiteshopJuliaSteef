ece27a91f1d303339be0bf82785adba8419d759d
Unknown <jdelozanne@gmail.com>
2017-12-13 11:15:03 +0100

    nieuw project met pom

---
+++ src/main/java/com/steefjulia/kiteshop/KiteshopApplication.java
@@ -0,0 +1,12 @@
+package com.steefjulia.kiteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiteshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiteshopApplication.class, args);
+	}
+}
