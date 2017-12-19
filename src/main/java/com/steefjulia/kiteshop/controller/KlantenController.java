d5e00a5be60ed0766419159e0126564a160cacfd
Unknown <jdelozanne@gmail.com>
2017-12-19 11:32:05 +0100

    sign up aangepast met account

--- src/main/java/com/steefjulia/kiteshop/controller/KlantenController.java
+++ src/main/java/com/steefjulia/kiteshop/controller/KlantenController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.steefjulia.kiteshop.model.data.AdresDao;
 import com.steefjulia.kiteshop.model.data.KlantenDao;
 import com.steefjulia.kiteshop.model.*;
+import com.steefjulia.kiteshop.model.data.AccountDao;
 
 
 
@@ -28,56 +29,70 @@ public class KlantenController {
 
 	@Autowired private KlantenDao klantenDao;
 	@Autowired private AdresDao adresDao;
+        @Autowired private AccountDao accountDao;
 
 	
-	@RequestMapping(value = "registreer", method = RequestMethod.GET)
-	public String displayKlantToevoegenForm(Model model){
+        @RequestMapping(value = "addAccount", method = RequestMethod.GET)
+	public String displayAddAccountForm(Model model){
 
-		model.addAttribute("title", "Registreer u nu");
-		model.addAttribute(new Klant());
-	
-		
-		return "klanten/registreer";
+		model.addAttribute("title", "Register");
+		model.addAttribute(new Account());
+		return "klanten/addAccount";
 	}
 
-	@RequestMapping(value = "registreer", method = RequestMethod.POST)
+	@RequestMapping(value = "addAccount", method = RequestMethod.POST)
+	public String processAddAccountForm(@ModelAttribute @Valid Account newAccount,
+			Errors errors, Model model, HttpServletRequest request){  
+
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Register");
+			model.addAttribute(errors);
+			return "klanten/addAccount";
+		}
+
+		HttpSession session = request.getSession();
+		session.setAttribute("account", newAccount);
+		//Account account = (Account) session.getAttribute("account");
+		return "redirect:/klanten/addCustomer";
+	}
+        
+	@RequestMapping(value = "addCustomer", method = RequestMethod.GET)
+	public String displayAadCustomerForm(Model model){
+
+		model.addAttribute("title", "Please enter the form");
+		model.addAttribute(new Klant());
+		return "klanten/addCustomer";
+	}
+
+	@RequestMapping(value = "addCustomer", method = RequestMethod.POST)
 	public String processKlantToevoegenForm(@ModelAttribute @Valid Klant newKlant,
 			Errors errors, Model model, HttpServletRequest request){  
-		
-		
-		
 		
 		if (errors.hasErrors()) {
 			model.addAttribute("title", "Registreer u nu");
 			model.addAttribute(errors);
-		
-			
-			return "klanten/registreer";
+                        return "klanten/addCustomer";
 		}
-		
-
+                
 		HttpSession session = request.getSession();
 		session.setAttribute("klant", newKlant);
-		
 		Klant klant = (Klant) session.getAttribute("klant");
-		System.out.println("Post klant"+klant);
 		
-		return "redirect:/klanten/voegAdresToe";
+		return "redirect:/klanten/addAddress";
 	}
 	
-	@RequestMapping(value = "voegAdresToe", method = RequestMethod.GET)
+	@RequestMapping(value = "addAddress", method = RequestMethod.GET)
 	public String displayAdresToevoegenForm(Model model){
 
 		
 		model.addAttribute("title", "Adres");
 		model.addAttribute(new Adres());
-		
-		
-		return "klanten/voegAdresToe";
+
+		return "klanten/addAddress";
 	}
 
 	
-	@RequestMapping(value = "voegAdresToe", method = RequestMethod.POST)
+	@RequestMapping(value = "addAddress", method = RequestMethod.POST)
 	public String processAddCheeseForm(@ModelAttribute @Valid Adres newAdres,
 			Errors errors, Model model, HttpServletRequest request){  
 		
@@ -89,16 +104,17 @@ public class KlantenController {
 			model.addAttribute(new Klant());
 			model.addAttribute(new Adres());
 			
-			return "klanten/registreer";
+			return "klanten/addCustomer";
 		}
 
 		HttpSession session = request.getSession();
 		Klant klant = (Klant) session.getAttribute("klant");
-		
-		adresDao.save(newAdres);
-		klant.setBezoekAdres(newAdres);
+		Account account = (Account) session.getAttribute("account");
+                klant.setBezoekAdres(newAdres);
+		account.setKlant(klant);
+                adresDao.save(newAdres);
 		klantenDao.save(klant);
-		System.out.println(klant);
+		accountDao.save(account);
 		return "redirect:/";
 	}
 	
--- src/main/java/com/steefjulia/kiteshop/model/Klant.java
+++ src/main/java/com/steefjulia/kiteshop/model/Klant.java
@@ -12,38 +12,44 @@ import java.io.Serializable;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+import org.hibernate.validator.constraints.Email;
 
 @Entity
 public class Klant implements Serializable {
-    
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private int klantID;
-	
-	@ManyToOne
+    
+    @OneToOne
+    @JoinColumn( name = "accountid")
+    private Account account;
+
+    @ManyToOne
     private Adres bezoekAdres;
-	
-	@ManyToOne
+
+    @ManyToOne
     private Adres factuurAdres;
-	
-	@NotNull
-	@Size(min=2, message = "Voornaam is een verplicht veld")
+
+    @NotNull
+    @Size(min = 2, message = "Voornaam is een verplicht veld")
     private String voornaam;
-	
+
     private String tussenvoegsel;
     private String achternaam;
+    @Email(message = "this is not a valid emailadress")
+    @Size(min = 1, message = "please give your emailadress")
     private String email;
     private String telefoonnummer;
-    
+
     public Klant() {
     }
-    
+
     public int getKlantID() {
         return klantID;
     }
-    
-    public void setKlantID(int id){
+
+    public void setKlantID(int id) {
         this.klantID = id;
     }
 
@@ -70,107 +76,125 @@ public class Klant implements Serializable {
     public void setAchternaam(String achternaam) {
         this.achternaam = achternaam;
     }
-    
+
     public String getEmail() {
-		return email;
-	}
+        return email;
+    }
 
-	public String getTelefoonnummer() {
-		return telefoonnummer;
-	}
+    public String getTelefoonnummer() {
+        return telefoonnummer;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setTelefoonnummer(String telefoonnummer) {
-		this.telefoonnummer = telefoonnummer;
-	}
-	
-	public Adres getBezoekAdres() {
-		return bezoekAdres;
-	}
+    public void setTelefoonnummer(String telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
 
-	public Adres getFactuurAdres() {
-		return factuurAdres;
-	}
+    public Adres getBezoekAdres() {
+        return bezoekAdres;
+    }
 
-	public void setBezoekAdres(Adres bezoekAdres) {
-		this.bezoekAdres = bezoekAdres;
-	}
+    public Adres getFactuurAdres() {
+        return factuurAdres;
+    }
 
-	public void setFactuurAdres(Adres factuurAdres) {
-		this.factuurAdres = factuurAdres;
-	}
+    public void setBezoekAdres(Adres bezoekAdres) {
+        this.bezoekAdres = bezoekAdres;
+    }
 
-	@Override
-	public String toString() {
-		return "Klant [klantID=" + klantID + ", bezoekAdres=" + bezoekAdres + ", factuurAdres=" + factuurAdres
-				+ ", voornaam=" + voornaam + ", tussenvoegsel=" + tussenvoegsel + ", achternaam=" + achternaam
-				+ ", email=" + email + ", telefoonnummer=" + telefoonnummer + "]";
-	}
+    public void setFactuurAdres(Adres factuurAdres) {
+        this.factuurAdres = factuurAdres;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((achternaam == null) ? 0 : achternaam.hashCode());
-		result = prime * result + ((bezoekAdres == null) ? 0 : bezoekAdres.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((factuurAdres == null) ? 0 : factuurAdres.hashCode());
-		result = prime * result + klantID;
-		result = prime * result + ((telefoonnummer == null) ? 0 : telefoonnummer.hashCode());
-		result = prime * result + ((tussenvoegsel == null) ? 0 : tussenvoegsel.hashCode());
-		result = prime * result + ((voornaam == null) ? 0 : voornaam.hashCode());
-		return result;
-	}
+    @Override
+    public String toString() {
+        return "Klant [klantID=" + klantID + ", bezoekAdres=" + bezoekAdres + ", factuurAdres=" + factuurAdres
+                + ", voornaam=" + voornaam + ", tussenvoegsel=" + tussenvoegsel + ", achternaam=" + achternaam
+                + ", email=" + email + ", telefoonnummer=" + telefoonnummer + "]";
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Klant other = (Klant) obj;
-		if (achternaam == null) {
-			if (other.achternaam != null)
-				return false;
-		} else if (!achternaam.equals(other.achternaam))
-			return false;
-		if (bezoekAdres == null) {
-			if (other.bezoekAdres != null)
-				return false;
-		} else if (!bezoekAdres.equals(other.bezoekAdres))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (factuurAdres == null) {
-			if (other.factuurAdres != null)
-				return false;
-		} else if (!factuurAdres.equals(other.factuurAdres))
-			return false;
-		if (klantID != other.klantID)
-			return false;
-		if (telefoonnummer == null) {
-			if (other.telefoonnummer != null)
-				return false;
-		} else if (!telefoonnummer.equals(other.telefoonnummer))
-			return false;
-		if (tussenvoegsel == null) {
-			if (other.tussenvoegsel != null)
-				return false;
-		} else if (!tussenvoegsel.equals(other.tussenvoegsel))
-			return false;
-		if (voornaam == null) {
-			if (other.voornaam != null)
-				return false;
-		} else if (!voornaam.equals(other.voornaam))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((achternaam == null) ? 0 : achternaam.hashCode());
+        result = prime * result + ((bezoekAdres == null) ? 0 : bezoekAdres.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((factuurAdres == null) ? 0 : factuurAdres.hashCode());
+        result = prime * result + klantID;
+        result = prime * result + ((telefoonnummer == null) ? 0 : telefoonnummer.hashCode());
+        result = prime * result + ((tussenvoegsel == null) ? 0 : tussenvoegsel.hashCode());
+        result = prime * result + ((voornaam == null) ? 0 : voornaam.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Klant other = (Klant) obj;
+        if (achternaam == null) {
+            if (other.achternaam != null) {
+                return false;
+            }
+        } else if (!achternaam.equals(other.achternaam)) {
+            return false;
+        }
+        if (bezoekAdres == null) {
+            if (other.bezoekAdres != null) {
+                return false;
+            }
+        } else if (!bezoekAdres.equals(other.bezoekAdres)) {
+            return false;
+        }
+        if (email == null) {
+            if (other.email != null) {
+                return false;
+            }
+        } else if (!email.equals(other.email)) {
+            return false;
+        }
+        if (factuurAdres == null) {
+            if (other.factuurAdres != null) {
+                return false;
+            }
+        } else if (!factuurAdres.equals(other.factuurAdres)) {
+            return false;
+        }
+        if (klantID != other.klantID) {
+            return false;
+        }
+        if (telefoonnummer == null) {
+            if (other.telefoonnummer != null) {
+                return false;
+            }
+        } else if (!telefoonnummer.equals(other.telefoonnummer)) {
+            return false;
+        }
+        if (tussenvoegsel == null) {
+            if (other.tussenvoegsel != null) {
+                return false;
+            }
+        } else if (!tussenvoegsel.equals(other.tussenvoegsel)) {
+            return false;
+        }
+        if (voornaam == null) {
+            if (other.voornaam != null) {
+                return false;
+            }
+        } else if (!voornaam.equals(other.voornaam)) {
+            return false;
+        }
+        return true;
+    }
 }
