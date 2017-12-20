bb45fac7f64af6b21dcce182f2298d48b720cdb8
SteefP <s.pelgrom@hotmail.com>
2017-12-20 12:01:13 +0100

    bestelling kalssen erbij

--- src/main/java/com/steefjulia/kiteshop/controller/BestellingController.java
+++ src/main/java/com/steefjulia/kiteshop/controller/BestellingController.java
@@ -3,17 +3,24 @@ package com.steefjulia.kiteshop.controller;
 import javax.servlet.http.HttpSession;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.steefjulia.kiteshop.model.*;
+import com.steefjulia.kiteshop.model.data.BestellingDao;
+import com.steefjulia.kiteshop.model.data.ProductDao;
 
 
 @Controller
 @RequestMapping("bestelling")
 public class BestellingController {
+	
+	
+	@Autowired
+    private BestellingDao bestellingDao;
 
 	@RequestMapping(value = "winkelmand", method = RequestMethod.GET)
 	public String showBestelling(Model model, HttpServletRequest request) {
@@ -26,29 +33,54 @@ public class BestellingController {
 		//
 		return "bestelling/winkelmand";
 	}
-	
+
 	@RequestMapping(value = "winkelmand", method = RequestMethod.POST)
 	public String gaNaarBetalen(HttpServletRequest request) {
-		
+
 		HttpSession session = request.getSession();
-	
-		Bestelling bestelling = (Bestelling) session.getAttribute("bestelling");
+
+
 
 		if(session.getAttribute("account")!=null){
 			return "redirect:/bestelling/afrekenen";
 		} else {
 			return "redirect:/klanten/addAccount";
 		}
-		
-		
+
+
 	}
 
 	@RequestMapping(value = "afrekenen", method = RequestMethod.GET)
 	public String showAfrekenen(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		model.addAttribute(session.getAttribute("bestelling"));
 
-		model.addAttribute(request.getSession().getAttribute("bestelling"));
-		
-		
+		Account account = (Account) session.getAttribute("account");
+		System.out.println(account);
+
+
+		Klant klant = account.getKlant();
+		System.out.println(klant);
+		model.addAttribute(klant);
+
 		return "bestelling/afrekenen";
+	}
+
+	@RequestMapping(value = "afrekenen", method = RequestMethod.POST)
+	public String gaNaarDankvoorUwAankoop(HttpServletRequest request) {
+
+		HttpSession session = request.getSession();
+		Bestelling bestelling = (Bestelling) session.getAttribute("bestelling");
+		Klant klant =  (Klant) session.getAttribute("klant");
+		bestelling.setKlant(klant);
+		bestellingDao.save(bestelling);
+		
+		if(session.getAttribute("account")!=null){
+			return "redirect:/bestelling/afrekenen";
+		} else {
+			return "redirect:/klanten/addAccount";
+		}
+
+
 	}
 }
---
+++ src/main/java/com/steefjulia/kiteshop/model/data/BestellingDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steefjulia.kiteshop.model.data;
+
+import com.steefjulia.kiteshop.model.Bestelling;
+import com.steefjulia.kiteshop.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+
+/**
+ *
+ * @author julia
+ */
+@Repository
+@Transactional
+public interface BestellingDao extends CrudRepository<Bestelling, Integer> {
+    
+}
