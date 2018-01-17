fb24391c47dc21003874a36cedcb02be0bec18fd
Unknown <jdelozanne@gmail.com>
2018-01-17 16:42:06 +0100

    nu ook knop pas enabled als je 2x zelfde password hebt

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -11,9 +11,11 @@
 
 function checkPass()
 {
+    
     //Store the password field objects into variables ...
     var pass1 = document.getElementById('pass1');
     var pass2 = document.getElementById('pass2');
+    var permission = document.getElementById('register');
     //Store the Confimation Message Object ...
     var message = document.getElementById('confirmMessage');
     //Set the colors we will be using ...
@@ -21,19 +23,22 @@ function checkPass()
     var badColor = "#ff6666";
     //Compare the values in the password field 
     //and the confirmation field
-    if(pass1.value == pass2.value){
+    if(pass1.value === pass2.value){
         //The passwords match. 
         //Set the color to the good color and inform
         //the user that they have entered the correct password 
+        
         pass2.style.backgroundColor = goodColor;
         message.style.color = goodColor;
-        message.innerHTML = "Passwords Match!"
+        message.innerHTML = "Passwords Match!";
+        permission.removeAttribute("disabled");
+        
     }else{
         //The passwords do not match.
         //Set the color to the bad color and
         //notify the user.
         pass2.style.backgroundColor = badColor;
         message.style.color = badColor;
-        message.innerHTML = "Passwords Do Not Match!"
+        message.innerHTML = "Passwords Do Not Match!";
     }
 }  
